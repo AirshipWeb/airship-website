fa5e8c49b511329f5012c1f92cd2aacb611a56a5
allisonprice <35505278+allisonprice@users.noreply.github.com>
2019-08-08 12:12:59 -0500

    Changed the link to the summit schedule

--- site/.vuepress/components/bar.vue
+++ site/.vuepress/components/bar.vue
@@ -7,7 +7,7 @@
           <p>Get involved with the Airship community at the Open Infrastructure Summit Shanghai!</p>
         </div>
         <!-- /.bar-entry -->
-        <a href="//airshipit.org/blog/airship-shanghai.html" target="_blank" class="bar-btn">View Sessions</a>
+        <a href="//openstack.org/summit/shanghai-2019/summit-schedule/global-search?t=airship" target="_blank" class="bar-btn">View Sessions</a>
       </div>
       <!-- /.bar-inner -->
     </div>
